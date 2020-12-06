26ae64480d035c7a566298da0d424340e9096696
JaswanthReddyPadala <jaswanthreddypadala@gmail.com>
2020-12-06 15:02:54 +0530

    add: catch error function

--- index.js
+++ index.js
@@ -3,11 +3,29 @@ var clearbtn = document.querySelector("#clearBtn");
 var pirate = document.querySelector(".pirate");
 var input = document.querySelector("#textInput");
 var output = document.querySelector(".output");
-translatepirate.addEventListener("click", function handleEvent(){
-    fetch("	https://api.funtranslations.com/translate/pirate.json?text="+input.value)
+var baseUrl = "https://api.funtranslations.com/translate/pirate.json?text=";
 
+function completeUrl(text){
+    return baseUrl+text;
+}
+
+function errorHandle(error){
+    pirate.style.display = "none";
+if (error.code==429){
+    alert("You crossed the hourly requests limit, please try after sometime.");
+}
+else{
+    console.log("Server issues: ", error);
+    alert("Something went wrong with our server, please try again after some time.")
+}
+}
+
+translatepirate.addEventListener("click", function handleEvent(){
+    fetch(completeUrl(input.value))
     .then(res => res.json())
-    .then(translatedText => output.textContent = translatedText["contents"]["translated"]);
+    .then(translatedText => {
+        output.textContent = translatedText.contents.translated;
+    }).catch(errorHandle);
     
     pirate.style.display = "block";
 });
